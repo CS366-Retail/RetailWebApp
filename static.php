685eeef199cf41b029240fa3a3d6fc9fa6fd5ce2
BuildTools <jtyler_@outlook.com>
2016-12-02 23:35:23 -0600

    Added .gitignore; db config template; style and static content foundation

---
+++ static.php
@@ -0,0 +1,17 @@
+<?php
+
+function printHead()
+    echo "<head>";
+    echo "<link rel='stylesheet' type='text/css' href='style/theme.css'>";
+    echo "</head>";
+end
+
+function printNav()
+    
+end
+
+function printFooter()
+
+end
+
+?>
