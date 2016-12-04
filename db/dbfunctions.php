4737355baffd4644e070e21d96b31e92544f1965
BuildTools <jtyler_@outlook.com>
2016-12-04 02:10:23 -0600

    dbfunctions runs now

--- db/dbfunctions.php
+++ db/dbfunctions.php
@@ -15,6 +15,9 @@ $TABLE_Sales="Sales";
 
 function createPercentCoupon($couponCode, $expiration, $maxQuantity, $percentDiscount, $inventoryItems)
 {
+  global $TABLE_CouponApplicableItems;
+  
+  $connection = connect();
 	$stmt1 = $connection->prepare("INSERT INTO Coupons (couponCode, expiration, maxQuantity, isPercentDiscount)
 	VALUES (?, ?, ?, ?)");
 	$stmt1->bind_param("ssii", $cc, $exp, $max, $isPercent);
@@ -23,33 +26,32 @@ function createPercentCoupon($couponCode, $expiration, $maxQuantity, $percentDis
 	$max = $maxQuantity;
 	$isPercent = $percentDiscount;
 	$stmt1->execute();
-	$stmt2 = $connection -> prepare("INSERT INTO $TABLE_CouponApplicableItems (couponCode, inventoryUPC)
-	VALUES (?,?)";
+	$stmt2 = $connection->prepare("INSERT INTO $TABLE_CouponApplicableItems (couponCode, inventoryUPC) VALUES (?,?)");
 	$stmt2 -> bind_param("ii", $code, $upc);
 	$code = $couponCode;
 	foreach ($upcs as $value)
 	{
 		$upc = $value;
-		stmt2->execute();
+		$stmt2->execute();
 	}
 }
 
 function createBSGSCoupon($couponCode, $expiration, $maxQuantity, $qualifyingQuantity, $getQuantity, $pricePercentOfGetItems, $inventoryItems)
-    
+{;}
 function expireCoupon($couponCode)
-    
+{;}
 function createInventoryItem($name, $price, $quantity)
-
+{;}
 function addInventoryQuantity($UPC)
-
+{;}
 function setInventoryPrice($UPC, $price)
-
+{;}
 function createSale($firstNameCustomer, $lastNameCustomer, $phoneNumberCustomer, $emailAddressCustomer, $employeeId, $inventoryItems, $quantities)
-
+{;}
 function createEmployee($firstName, $lastName, $username, $password, $pin)
-
+{;}
 function validateEmployeePassword($username, $password)
-
+{;}
 function validateEmployeePin($username, $pin)
-
+{ return true; }
 ?>
